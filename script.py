12b01c149d161836a2ce7b6f5501c89d335acc7a
sujal <ss6sujal@gmail.com>
2023-04-23 16:12:52 +0400

    jupyter

--- script.py
+++ script.py
@@ -10,6 +10,7 @@ import requests
 # constant definitions
 API_KEY = os.getenv('TORN_API_KEY_ENV')
 SCORES_FILE_NAME = "output.csv"
+SCORES_FILE_NAME_FORMATTED = "output_formatted.csv"
 MASTER_FILE_NAME = "all_attacks.csv"
 ASSISTS_KEY = 'assist'
 ATTACKS_KEY = 'attacks'
@@ -18,6 +19,7 @@ NAME_KEY = 'player'
 BONUS_RESPECT_KEY = 'bonus_hit_respect'
 POINTS_LOST = 'points_lost'
 POINTS_GAINED = 'points_gained'
+PAYOUT_KEY = 'payout'
 
 CODE = 'code'
 TIMESTAMP_STARTED = 'timestamp_started'
@@ -55,9 +57,32 @@ extra_calls = [',applications', ',armor', ',armorynews', ',attacknews',
                ]
 
 
-def json_to_csv(json_list, csv_filename):
+def csv_to_json(csv_filename):
+    # Read the CSV file and store the data in a list of dictionaries
+    with open(csv_filename, "r", newline="") as csvfile:
+        csv_reader = csv.DictReader(csvfile)
+        data = [row for row in csv_reader]
+    return data
+
+
+def json_to_csv(json_list, csv_filename, sort_by=None, format_attribute=None, formatter=None):
     # Get headers from the first dictionary in the list
     headers = list(json_list[0].keys())
+
+    should_format = False
+    format_index = -1
+    if format_attribute and formatter:
+        should_format = True
+        for index, header in enumerate(headers):
+            if header == format_attribute:
+                format_index = index
+
+    # Sort the list by the given column
+    if sort_by is not None:
+        if sort_by in headers:
+            json_list.sort(key=lambda x: x[sort_by], reverse=True)
+        else:
+            print(f"Column '{sort_by}' not found. Saving without sorting.")
 
     # Open a CSV file for writing
     with open(csv_filename, "w", newline="") as csvfile:
@@ -70,25 +95,44 @@ def json_to_csv(json_list, csv_filename):
         # Write the data rows
         for record in json_list:
             row = list(record.values())
+            # formatter
+            if should_format:
+                formatted_row = row
+                formatted_row[format_index] = formatter(formatted_row[format_index])
+                row = formatted_row
+
             csv_writer.writerow(row)
-    print(f"file with name {csv_filename} saved.")
+    print(f"File with name {csv_filename} saved.")
 
 
-def visualize_report():
-    # Create a dataframe from the data
-    df = pd.read_csv('output.csv')
+def table_scores(df):
+    # Plot the table
+    fig, ax = plt.subplots(figsize=(12, 5))
+    ax.axis('off')
+    ax.table(cellText=df.values, colLabels=df.columns, loc='center', cellLoc='center')
+    plt.show()
 
-    # Sort the dataframe by the "attacks" column
-    df = df.sort_values(by=ATTACKS_KEY, ascending=False)
+
+def graph_top10(df):
+    # Get the top 10 players by points gained
+    top10 = df.sort_values(by=POINTS_GAINED, ascending=False).head(10)
+
+    # Get names and points gained values
+    names = top10[NAME_KEY]
+    points_gained = top10[POINTS_GAINED]
+
+    # Create the bar graph
     plt.figure(figsize=(12, 6))
-    # bars = plt.bar(df['name'], df['attacks'], df['lost'], df['outside_attacks'])
-
-    # Create a bar chart of the "attacks" column
-    plt.bar(df[NAME_KEY], df[ATTACKS_KEY], width=0.5)
+    plt.bar(names, points_gained, width=0.5)
     plt.xticks(rotation=90)
     plt.xlabel('Name')
-    plt.ylabel('Attacks')
-    plt.title('Attacks by Name')
+    plt.ylabel('Points Gained')
+    plt.title('Top 10 Players by Points Gained')
+
+
+def bleeders_graph(_df):
+    # Sort the dataframe by the "attacks" column
+    df = _df.sort_values(by=ATTACKS_KEY, ascending=False)
 
     # Get the bleeders
     bleeders = df[df[POINTS_LOST] > df[POINTS_GAINED]].sort_values(by=POINTS_LOST, ascending=False)
@@ -104,6 +148,7 @@ def visualize_report():
     plt.xlabel('Name')
     plt.ylabel('Points Lost')
     plt.title('Bleeders')
+
     plt.show()
 
 
@@ -140,15 +185,18 @@ def flatten_json(json_record, separator='_', prefix=''):
     return flattened_record
 
 
-def csv_to_json(csv_filename):
-    # Read the CSV file and store the data in a list of dictionaries
-    with open(csv_filename, "r", newline="") as csvfile:
-        csv_reader = csv.DictReader(csvfile)
-        data = [row for row in csv_reader]
-    return data
+def format_payout(num):
+    if num >= 100000000:
+        return f"{num / 1000000:.0f}M"
+    elif num >= 1000000:
+        return f"{num / 1000000:.1f}M" if num % 1000000 != 0 else f"{num // 1000000}M"
+    elif num >= 100000:
+        return f"{num / 1000:.0f}k"
+    else:
+        return str(num)
 
 
-### LOAD THE LIST OF ATTACKS ###
+# LOAD THE LIST OF ATTACKS
 def load_list():
     try:
         with open(MASTER_FILE_NAME, "r"):
@@ -159,7 +207,33 @@ def load_list():
         return None
 
 
-### PROCESS THE ATTACK ROWS ###
+def process_payout(scores, budget):
+    # Calculate the total number of weighted actions
+    total_weighted_actions = sum(
+        player[ATTACKS_KEY] * ATTACKS_WEIGHT +
+        player[ASSISTS_KEY] * ASSISTS_WEIGHT +
+        player[OUTSIDE_ATTACKS_KEY] * OUTSIDE_ATTACKS_WEIGHT
+        for player in scores
+    )
+
+    # Calculate the payout for each player based on the weighted values
+    for player in scores:
+        weighted_actions = (
+                player[ATTACKS_KEY] * ATTACKS_WEIGHT +
+                player[ASSISTS_KEY] * ASSISTS_WEIGHT +
+                player[OUTSIDE_ATTACKS_KEY] * OUTSIDE_ATTACKS_WEIGHT
+        )
+        # update the payout
+        player[PAYOUT_KEY] = round(budget / total_weighted_actions * weighted_actions)
+        # round off the decimals
+        player[POINTS_GAINED] = round(player[POINTS_GAINED])
+        player[POINTS_LOST] = round(player[POINTS_LOST])
+
+    total_paid = sum(player[PAYOUT_KEY] for player in scores)
+    print(f"total paid : {total_paid}")
+
+
+# PROCESS THE ATTACK ROWS
 def process_attacks(hero_faction_name, enemy_faction_name, master_list):
     print(f"going to process total {len(master_list)} records.")
 
@@ -265,7 +339,7 @@ def process_attacks(hero_faction_name, enemy_faction_name, master_list):
     return output
 
 
-#### CALL API TO LOAD ATTACKS ###
+# CALL API TO LOAD ATTACKS
 def fetch_attacks(end_stamp, start_stamp, api_key):
     attacks_master_list = []
     next_call_stamp = start_stamp
@@ -305,8 +379,8 @@ def fetch_attacks(end_stamp, start_stamp, api_key):
     return attacks_master_list
 
 
-### GENERATE THE REPORT
-def generate_report(hero_faction_name, enemy_faction_name, duration_txt, api_key):
+# GENERATE THE REPORT
+def generate_report(hero_faction_name, enemy_faction_name, duration_txt, budget, api_key):
     if api_key is not None:
         masked_api_key = "*****" + api_key[-5:]
         print("Using API Key:", masked_api_key)
@@ -323,13 +397,29 @@ def generate_report(hero_faction_name, enemy_faction_name, duration_txt, api_key
         json_to_csv(attacks_master_list, MASTER_FILE_NAME)
     # step 2 - process attacks
     scores = process_attacks(hero_faction_name, enemy_faction_name, attacks_master_list)
-    json_to_csv(scores, SCORES_FILE_NAME)
+    # step 3 - process payouts
+    process_payout(scores, budget)
+    json_to_csv(scores, SCORES_FILE_NAME, sort_by=PAYOUT_KEY)
+    json_to_csv(scores, SCORES_FILE_NAME_FORMATTED, sort_by=PAYOUT_KEY, format_attribute=PAYOUT_KEY,
+                formatter=format_payout)
 
+
+# weights of the hit according to hit type
+
+ATTACKS_WEIGHT = 1.0
+ASSISTS_WEIGHT = 0.4
+OUTSIDE_ATTACKS_WEIGHT = 0.75
 
 generate_report(
     hero_faction_name="The Power Rangers",
-    enemy_faction_name="Drunk Squad",
-    duration_txt="15:00:00 - 07/04/23 until 13:03:05 - 08/04/23",
+    enemy_faction_name="Revolutionary Guerrillas",
+    duration_txt="15:00:00 - 21/04/23 until 13:39:15 - 22/04/23",
+    budget=1_700_000_000,
     api_key=API_KEY)
 
-visualize_report()
+# Create a dataframe from the data
+# raw_data = pd.read_csv(SCORES_FILE_NAME)
+
+table_scores(pd.read_csv(SCORES_FILE_NAME_FORMATTED))
+# graph_top10(data)
+# bleeders_graph(data)
